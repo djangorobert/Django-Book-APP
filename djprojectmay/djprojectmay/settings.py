8613c6f8713f69233ac68b3a1248cfbc5fadf3e2
Robert Anchondo <djangocharm2020@gmail.com>
2018-05-20 17:50:51 -0500

    Update settings.py

--- djprojectmay/djprojectmay/settings.py
+++ djprojectmay/djprojectmay/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!9rm!_@b8vgo-&#d3)w^s211hej)78ztk-!oir^ff+szz+6642'
+SECRET_KEY = 'your secrect key here'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [127.0.0.1:8000]
 
 
 # Application definition
